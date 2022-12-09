33c007d4b21f93b5dfd3b0b5adf8e20e737bccc6
Vahit Keskin <vahit.keskin@appcent.mobi>
2022-12-09 17:36:48 +0300

    unnecessary import and deletion of non-working codes

--- app/src/main/java/com/vahitkeskin/jetpackcomposewikipediaclone/component/WikipediaTopAppBar.kt
+++ app/src/main/java/com/vahitkeskin/jetpackcomposewikipediaclone/component/WikipediaTopAppBar.kt
@@ -1,19 +1,14 @@
 package com.vahitkeskin.jetpackcomposewikipediaclone.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import com.vahitkeskin.jetpackcomposewikipediaclone.R
 
 @Composable
@@ -42,14 +37,5 @@ fun WikipediaTopAppBar() {
                 )
             }
         }
-        /*
-            Icon(
-                modifier = Modifier.clickable {
-                    navHostController.popBackStack()
-                },
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = null
-            )
-        */
     }
 }
